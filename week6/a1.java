f6ad738d768b144f202f0b944b43d6b651635829
kkkhhhzzz0 <1760291167@qq.com>
2024-10-14 10:29:26 +0800

    Add files via upload

---
+++ week6/a1.java
@@ -0,0 +1,9 @@
+public class a1{
+    public static void main(String[] args){
+    int sum=0,i=1;
+    while(i<=9){
+        sum=sum+i;
+        i++;
+    }
+    System.out.println("sum is"+sum);
+    }}
---
+++ week6/a7.java
@@ -0,0 +1,13 @@
+public class a7
+ { 
+
+public static void main(String[] args)
+     { int i;int sum = 0; for ( i = 0; i < 10; i++) 
+        { sum += i; } 
+        int j = 0;
+         if (i < j)
+         { System.out.println(i); }
+          else { System.out.println(j); } 
+          while (j < 10)
+           { j++; } 
+            System.out.println("Sum: " + sum); } }
